6c3ecd9c12183d74f01e5d1a7870b115c6b48349
Herrity <eherrity@wm.edu>
2020-09-08 14:29:21 -0700

    even more pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,29 +1,33 @@
 import os
 import csv
 
-pybank = os.path.join("Resources", "budget_data.csv")
+pybank = os.path.join("..","Resources", "budget_data.csv")
 
-with open(pybank, 'r') as csvfile:
+with open(pybank) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
     revenue = []
     date = []
-    rev_change[]
+    rev_change = []
 
-    revenue.append(float(row[1]))
-    date.append(row[0])
+    revenue.append(float(pybank[1]))
+    date.append(pybank[0])
 
     print("Financial Analysis")
     print("----------------------------")
-    print(f"Total Months:" len(date))
-    print(f"Total:", sum(revenue))
+    print("Total Months:", len(date))
+    print("Total: $", sum(revenue))
 
-  for i in range(1,len(revenue)):
-      rev_change.append(revenue[i] - revenue[i - 1])
-      
+    for i in range(1,len(revenue)):
+        rev_change.append(revenue[i] - revenue[i - 1])
+        avg_rev_change = sum(rev_change)/len(rev_change)
+        max_rev_change = max(rev_change)
+        min_rev_change = min(rev_change)
+        max_rev_change_date = str(date[rev_change.index(max(rev_change))])
+        min_rev_change_date = str(date[rev_change.index(min(rev_change))])
 
+    print(f"Average Change: $", round(avg_rev_change))
+    print(f"Greatest Increase in Profits:", max_rev_change_date, "($", max_rev_change,")")
+    print(f"Greatest Decrease in Profits:", min_rev_change_date,"($", min_rev_change,")")
 
-    print(f"Average Change: {str(average_pl)}")
-    print(f"Greatest Increase in Profits: {str(increase)}")
-    print(f"Greatest Decrease in Profits: {str(decrease)}")
